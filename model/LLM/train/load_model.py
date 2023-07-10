831946ec4e4bf266133b8908e31d18f3c35aafdf
taemin6697 <taemin6697@naver.com>
2023-07-10 22:20:37 +0900

    git commit -am "feat: upload LLM source code
    - LLM train code
    - LLM inference code
    - LLM data preprocessing code
    
    #7"

---
+++ model/LLM/train/load_model.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+from utils import print_trainable_parameters
+def load_model(model_name):
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map={"": 0})
+    model.gradient_checkpointing_enable()
+    model = prepare_model_for_kbit_training(model)
+
+    config = LoraConfig(
+        r=8,
+        lora_alpha=32,
+        target_modules=["query_key_value"],
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM"
+    )
+
+    model = get_peft_model(model, config)
+    print_trainable_parameters(model)
+
+    return model,tokenizer
