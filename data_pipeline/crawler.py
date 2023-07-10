f81ef2a8695f90e922bd80945e05fe1c510d66da
MinJae Kang <kminjae618@gmail.com>
2023-07-11 04:01:52 +0900

    refactor: crawler.py
    #5

--- data_pipeline/crawler.py
+++ data_pipeline/crawler.py
@@ -1,6 +1,5 @@
 import pickle
 import re
-import time
 from urllib.request import urlopen
 
 import pandas as pd
@@ -13,102 +12,136 @@ from tqdm.auto import tqdm
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_case_id(driver):
-    case_ids = []
-    element_xpath = "//a[contains(@onclick, 'fn_inquire_detail')]"
-    WebDriverWait(driver, 100).until(
-        EC.presence_of_element_located((By.XPATH, element_xpath))
-    )
-    elements = driver.find_elements(By.XPATH, element_xpath)
-    for element in elements:
-        match = re.search(
-            r"fn_inquire_detail\('(\d+)', '(.*?)'\);return false;",
-            element.get_attribute("onclick"),
+class QADataCrawler:
+    def __init__(
+        self,
+        board_url="https://www.klac.or.kr/legalstruct/cyberConsultation/selectOpenArticleList.do?boardCode=3#none",
+        base_url="https://www.klac.or.kr/legalstruct/cyberConsultation/selectOpenArticleDetail.do?boardCode=3&contentId=",
+    ):
+        self.driver = None
+        self.board_url = board_url
+        self.base_url = base_url
+
+    def start_driver(self):
+        self.chrome_service = webdriver.chrome.service.Service(
+            ChromeDriverManager().install()
         )
-        case_ids.append(match.group(2))
-    return case_ids
+        self.chrome_options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(
+            service=self.chrome_service, options=self.chrome_options
+        )
 
+        self.chrome_service.start()
 
-def save_case_id_list(case_id_list, file_name="case_id_list.pkl"):
-    with open(file_name, "wb") as f:
-        pickle.dump(case_id_list, f)
+    def quit_driver(self):
+        self.driver.quit()
 
+    def get_data(self):
+        case_ids = self._get_all_case_ids()
+        case_info = []
 
-def load_case_id_list(file="case_id_list.pkl"):
-    with open(file, "rb") as f:
-        loaded_list = pickle.load(f)
-    return loaded_list
+        for case_id in tqdm(case_ids):
+            case_info.append(self._get_case_content_by_id(case_id))
 
+        self._save_dataframe(case_info)
 
-def get_case_info_by_id(base_url, id):
-    url = base_url + id
-    html = urlopen(url)
-    bsObject = BeautifulSoup(html, "html.parser")
-
-    case_title = bsObject.find("div", {"class": "view_head"}).text
-    date_created = bsObject.find("dt", text="신청일").find_next_sibling("dd").text
-    date_answered = bsObject.find("dt", text="답변일자").find_next_sibling("dd").text
-    content, answer = bsObject.find_all("div", {"class": "notice_contents"})
-    case_info = [case_title, date_created, date_answered, content.text, answer.text]
-
-    return case_info
-
-
-def main():
-    board_url = "https://www.klac.or.kr/legalstruct/cyberConsultation/selectOpenArticleList.do?boardCode=3#none"
-    base_url = "https://www.klac.or.kr/legalstruct/cyberConsultation/selectOpenArticleDetail.do?boardCode=3&contentId="
-
-    chrome_service = webdriver.chrome.service.Service(ChromeDriverManager().install())
-    chrome_service.start()
-    chrome_options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
-    driver.get(board_url)
-
-    case_id_list = []
-    case_id_list.extend(get_case_id(driver))
-
-    page_number = 1
-    print(f"Crawling of page {page_number} is finished.")
-
-    page_idx = 2
-    page_move_cnt = 0
-
-    while True:
-        # test
-        if page_move_cnt == 1: break
-        try:
-            next_page = driver.find_element(
-                By.XPATH, f'//*[@id="content"]/form[1]/div[2]/div/div[4]/a[{page_idx}]'
+    def _get_case_id(self):
+        case_ids = []
+        element_xpath = "//a[contains(@onclick, 'fn_inquire_detail')]"
+        WebDriverWait(self.driver, 100).until(
+            EC.presence_of_element_located((By.XPATH, element_xpath))
+        )
+        elements = self.driver.find_elements(By.XPATH, element_xpath)
+        for element in elements:
+            match = re.search(
+                r"fn_inquire_detail\('(\d+)', '(.*?)'\);return false;",
+                element.get_attribute("onclick"),
             )
-            page_idx += 1
-        except:
-            try:
-                next_page = driver.find_element(
-                    By.XPATH,
-                    f'//button[contains(@onclick, "fn_select_linkPage({page_move_cnt * 10 + page_idx}); return false;")]',
-                )
-                page_idx = 2
-                page_move_cnt += 1
-            except:
-                print("Crawling Finished.")
+            case_ids.append(match.group(2))
+        return case_ids
+
+    def _get_all_case_ids(self, save_id_list=False):
+        page_move_cnt = 0
+        page_number = 1
+        page_idx = 2
+        case_ids = []
+
+        self.driver.get(self.board_url)
+        case_ids.extend(self._get_case_id())
+
+        while True:
+            # test
+            if page_move_cnt == 1:
                 break
+            try:
+                next_page = self.driver.find_element(
+                    By.XPATH,
+                    f'//*[@id="content"]/form[1]/div[2]/div/div[4]/a[{page_idx}]',
+                )
+                page_idx += 1
+            except:
+                try:
+                    next_page = self.driver.find_element(
+                        By.XPATH,
+                        f'//button[contains(@onclick, "fn_select_linkPage({page_move_cnt * 10 + page_idx}); return false;")]',
+                    )
+                    page_idx = 2
+                    page_move_cnt += 1
+                except:
+                    print("End of Boards.")
+                    break
 
-        next_page.click()
-        case_id_list.extend(get_case_id(driver))
-        page_number += 1
-        print(f"Crawling of page {page_number} is finished.")
+            next_page.click()
+            case_ids.extend(self._get_case_id())
+            page_number += 1
 
-    save_case_id_list(case_id_list, "case_id_list.pkl")
+            if save_id_list:
+                self._save_case_id_list(case_ids, "case_id_list.pkl")
 
-    driver.quit()
+        return case_ids
 
-    case_info = []
-    for case_id in tqdm(case_id_list):
-        case_info.append(get_case_info_by_id(base_url, case_id))
+    def _get_case_content_by_id(self, case_id):
+        url = self.base_url + case_id
+        html = urlopen(url)
+        bsObject = BeautifulSoup(html, "html.parser")
 
-    df = pd.DataFrame(case_info, columns=["case_title", "date_created", "date_answered", "content", "answer"])
-    df.to_csv("raw_qa_dataset.csv", index=False)
+        case_title = bsObject.find("div", {"class": "view_head"}).text
+        date_created = bsObject.find("dt", text="신청일").find_next_sibling("dd").text
+        date_answered = bsObject.find("dt", text="답변일자").find_next_sibling("dd").text
+        content, answer = bsObject.find_all("div", {"class": "notice_contents"})
+        case_info = [case_title, date_created, date_answered, content.text, answer.text]
+
+        return case_info
+
+    def _save_dataframe(self, case_info, drop_unused_columns=False):
+        df = pd.DataFrame(
+            case_info,
+            columns=[
+                "case_title",
+                "date_created",
+                "date_answered",
+                "content",
+                "answer",
+            ],
+        )
+
+        if drop_unused_columns:
+            df = df.drop(["case_title", "date_created", "date_answered"], axis=1)
+
+        df.to_csv("raw_qa_dataset.csv", index=False)
+
+    def _save_case_id_list(self, case_id_list, file_name="case_id_list.pkl"):
+        with open(file_name, "wb") as f:
+            pickle.dump(case_id_list, f)
+
+    def _load_case_id_list(self, file="case_id_list.pkl"):
+        with open(file, "rb") as f:
+            loaded_list = pickle.load(f)
+        return loaded_list
+
 
 if __name__ == "__main__":
-    main()
+    crawler = QADataCrawler()
+    crawler.start_driver()
+    crawler.get_data()
+    crawler.quit_driver()
