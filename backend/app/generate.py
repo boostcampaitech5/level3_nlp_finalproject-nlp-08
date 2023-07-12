cb6999a49e2ad4ac978b49779f5667b899f80f63
SangwonYoon <yoonsw0532@naver.com>
2023-07-12 08:00:31 +0000

    feat: add function to search similar precedent
    #3

--- backend/app/generate.py
+++ backend/app/generate.py
@@ -1,23 +1,31 @@
+import time
+
 import torch
 
 
 def generate_answer(q_sentence: str, model, tokenizer):
     model.eval()
     model.config.use_cache = True  # silence the warnings. Please re-enable for inference!
-    model.float()
+    model.half()
     tokenizer.pad_token = tokenizer.eos_token
 
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    prompt = f"아래는 작업을 설명하는 명령어입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{q_sentence}\n\n### 응답:\n"
+    len_prompt = len(prompt)
+
+    start_time = time.time()
 
     gened = model.generate(
         **tokenizer(
-            f"### 질문: {q_sentence}\n\n### 답변:",
+            prompt,
             return_tensors='pt',
             return_token_type_ids=False
         ).to(device),
-        max_new_tokens=256,
+        max_new_tokens=512,
         early_stopping=True,
         do_sample=True,
         eos_token_id=2,
     )
-    return tokenizer.decode(gened[0])
+
+    print(f"generate time: {time.time() - start_time}")
+    return tokenizer.decode(gened[0])[len_prompt:].replace("<|endoftext|>", "")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,62 @@
 import os
+from typing import List
 
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI
+from peft import PeftConfig, PeftModel
 from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from generate import generate_answer
+from search import Precedent, load_vector_data, search_precedent
 
 
 class Question(BaseModel):
     q_sentence: str
 
+class Answer(BaseModel):
+    answer_sentence: str
+    similar_precedent: List[Precedent]
+
+
 app = FastAPI()
 
-model = None
+llm = None
 tokenizer = None
+search_model =  None
+text_data = None
+vector_data = None
 
 @app.on_event("startup")
 def startup_event():
-    global tokenizer, model
-    model_id = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "model")
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id, device_map={"":0})
+    global tokenizer, llm, search_model, text_data, vector_data
 
-@app.post("/generate")
+    print("Load LLM")
+    peft_model_id = "kfkas/LawBot-v1_koalpaca_legalQA_easylaw"
+    config = PeftConfig.from_pretrained(peft_model_id)
+    llm = AutoModelForCausalLM.from_pretrained(
+        config.base_model_name_or_path, device_map={"": 0}
+    )
+    llm = PeftModel.from_pretrained(llm, peft_model_id)
+    tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+
+    print("Load search model")
+    search_model = SentenceTransformer("jhgan/ko-sroberta-multitask")
+
+    print("Load data")
+    base_path = os.path.join(os.path.dirname(__file__))
+
+    text_data = np.array(pd.read_csv(base_path + "/../data/law_data/law_data_drop.csv"))
+    vector_data = load_vector_data(
+        base_path + "/../data/law_data/law_data_drop_vector.bin"
+    )
+
+
+@app.post("/generate", response_model=Answer)
 async def generate(question: Question):
-    return generate_answer(question.q_sentence, model, tokenizer)
+    q_sentence = question.q_sentence
+    answer_sentence = generate_answer(q_sentence=q_sentence, model=llm, tokenizer=tokenizer)
+    similar_precedent = search_precedent(q_a_sentence=q_sentence+answer_sentence, model=search_model, text_data=text_data, vector_data=vector_data)
+    return Answer(answer_sentence=answer_sentence, similar_precedent=similar_precedent)
