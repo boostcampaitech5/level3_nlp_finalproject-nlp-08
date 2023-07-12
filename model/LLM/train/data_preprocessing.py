9d9146d6eb6a5b84506d919c02026b6921152ad3
taemin6697 <taemin6697@hansung.ac.kr>
2023-07-12 10:50:12 +0900

    git commit -am "refactor: apply black formatting
    - 주석 제거
    - black formatting 적용
    
    #7"

--- model/LLM/train/data_preprocessing.py
+++ model/LLM/train/data_preprocessing.py
@@ -2,14 +2,17 @@ import pandas as pd
 from datasets import concatenate_datasets
 from datasets import load_dataset
 import os
-class Autodata():
-    def __init__(self,data_folder='./data',max_length=1024,tokenizer=None):
+
+
+class Autodata:
+    def __init__(self, data_folder="./data", max_length=1024, tokenizer=None):
         self.data_foloder = data_folder
         self.max_length = max_length
         self.tokenizer = tokenizer
         self.concat_dataset = self.concat_datasets(self.data_foloder)
         self.tokenizer_dataset = self.tokenizing_dataset(self.concat_dataset)
-    def concat_datasets(self,folder_path):
+
+    def concat_datasets(self, folder_path):
         datasets = []
         for file_name in os.listdir(folder_path):
             if file_name.endswith(".csv"):
@@ -18,18 +21,23 @@ class Autodata():
                 datasets.append(dataset["train"])
 
         combined_dataset = concatenate_datasets(datasets)
-        data = combined_dataset.remove_columns('title')
+        data = combined_dataset.remove_columns("title")
         return data
 
-    def tokenizing_dataset(self,dataset):
+    def tokenizing_dataset(self, dataset):
         data = dataset.map(
             lambda x: {
-                'text': f"아래는 작업을 설명하는 명령어입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{x['question']}\n\n### 응답:\n{x['answer']}"}
+                "text": f"아래는 작업을 설명하는 명령어입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{x['question']}\n\n### 응답:\n{x['answer']}"
+            }
         )
-        data = data.map(lambda samples: self.tokenizer(samples["text"], truncation=True,
-                                                  max_length=self.max_length,
-                                                  padding=False,
-                                                  return_tensors=None, ), batched=True)
+        data = data.map(
+            lambda samples: self.tokenizer(
+                samples["text"],
+                truncation=True,
+                max_length=self.max_length,
+                padding=False,
+                return_tensors=None,
+            ),
+            batched=True,
+        )
         return data.shuffle()
-
-
--- model/LLM/train/load_model.py
+++ model/LLM/train/load_model.py
@@ -3,15 +3,19 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import prepare_model_for_kbit_training
 from peft import LoraConfig, get_peft_model
 from utils import print_trainable_parameters
+
+
 def load_model(model_name):
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
+        bnb_4bit_compute_dtype=torch.bfloat16,
     )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map={"": 0})
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, quantization_config=bnb_config, device_map={"": 0}
+    )
     model.gradient_checkpointing_enable()
     model = prepare_model_for_kbit_training(model)
 
@@ -21,10 +25,10 @@ def load_model(model_name):
         target_modules=["query_key_value"],
         lora_dropout=0.05,
         bias="none",
-        task_type="CAUSAL_LM"
+        task_type="CAUSAL_LM",
     )
 
     model = get_peft_model(model, config)
     print_trainable_parameters(model)
 
-    return model,tokenizer
+    return model, tokenizer
