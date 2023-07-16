56102230d59611c5a36145a38f62864287e76e74
Kim-Ju-won <kjwt1124@icloud.com>
2023-07-16 17:18:51 +0000

    refactor: similar precedents component
    #1

--- prototype/src/components/SimilarPrecedentComponents/PrecedentCard.js
+++ prototype/src/components/SimilarPrecedentComponents/PrecedentCard.js
@@ -1,55 +1,17 @@
-function PrecedentCard() {
+function PrecedentCard({ precedent }) {
     return (
-        <div class="w-full overflow-x-hidden">
-            <aside id="separator-sidebar" class="fixed top-12 right-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <ul class="space-y-2 font-medium mt-2 mb-2">
-                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">유사 판례 조항 1</h5>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 케이스 : 재산분할</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 번호 : 2000스13</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 분류 : 가사</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">관련 법 조항 : 민법 제843조</p>
-                            <a href="https://sangwonyoon.tistory.com/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
-                        </div>
-                    </ul>
-                    <ul class="space-y-2 font-medium mt-2 mb-2">
-                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">유사 판례 조항 2</h5>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 케이스 : 이혼 및 위자료·재산분할·이혼등</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 번호 : 2009므2628,2635</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 분류 : 가사</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">관련 법 조항 : 민법 제839조의2 제2항</p>
-                            <a href="https://sangwonyoon.tistory.com/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
-                        </div>
-                    </ul>
-                    <ul class="space-y-2 font-medium mt-2 mb-2">
-                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">유사 판례 조항 3</h5>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 케이스 : 재산분할</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 번호 : 2012므2888</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">사건 분류 : 가사</p>
-                            <p class="font-small text-gray-700 dark:text-gray-400">관련 법 조항 : 민법 제843조</p>
-                            <a href="https://sangwonyoon.tistory.com/" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
-                        </div>
-                    </ul>
-                </div>
-            </aside>
+        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">유사 판례 조항 3</h5>
+            <p class="font-small text-gray-700 dark:text-gray-400">사건 케이스 : {precedent.case_name}</p>
+            <p class="font-small text-gray-700 dark:text-gray-400">사건 번호 : {precedent.case_number}</p>
+            <p class="font-small text-gray-700 dark:text-gray-400">사건 분류 : {precedent.case_type}</p>
+            <p class="font-small text-gray-700 dark:text-gray-400">관련 법 조항 :{precedent.ref_article}</p>
+            <a href={precedent.url} class="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+            </a>
         </div>
     )
 }
