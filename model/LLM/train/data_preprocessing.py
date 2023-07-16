b16d30449cfe0e373d670f38d97fc2409d517a71
taemin6697 <taemin6697@naver.com>
2023-07-17 00:02:37 +0900

    fix: LLM train코드에 eval 코드추가 및 데이터 전처리 수정
    - <|endoftext|>을 토크나이징시 텍스트 단위에서의 삽입
    - 훈련시 eval_data 평가
    - concat_dataset을 만들때 자동으로 question,answer의 column을 가져오도록 수정
    #26

--- model/LLM/train/data_preprocessing.py
+++ model/LLM/train/data_preprocessing.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from datasets import concatenate_datasets
+from datasets import concatenate_datasets, Dataset
 from datasets import load_dataset
 import os
 
 
-class Autodata:
-    def __init__(self, data_folder="./data", max_length=1024, tokenizer=None):
+class Autodata():
+    def __init__(self, data_folder='./data', max_length=1024, tokenizer=None):
         self.data_foloder = data_folder
         self.max_length = max_length
         self.tokenizer = tokenizer
@@ -17,27 +17,26 @@ class Autodata:
         for file_name in os.listdir(folder_path):
             if file_name.endswith(".csv"):
                 file_path = os.path.join(folder_path, file_name)
-                dataset = load_dataset("csv", data_files=file_path)
-                datasets.append(dataset["train"])
+                dataset = pd.read_csv(file_path)
+                dataframe = dataset[['question', 'answer']]
+                dataset = Dataset.from_pandas(dataframe)
+                datasets.append(dataset)
 
         combined_dataset = concatenate_datasets(datasets)
-        data = combined_dataset.remove_columns("title")
-        return data
+
+        return combined_dataset
 
     def tokenizing_dataset(self, dataset):
         data = dataset.map(
             lambda x: {
-                "text": f"아래는 작업을 설명하는 명령어입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{x['question']}\n\n### 응답:\n{x['answer']}"
+                "text": f"아래는 작업을 설명하는 명령어입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{x['question']}\n\n### 응답:\n{x['answer']}<|endoftext|>"
             }
         )
-        data = data.map(
-            lambda samples: self.tokenizer(
-                samples["text"],
-                truncation=True,
-                max_length=self.max_length,
-                padding=False,
-                return_tensors=None,
-            ),
-            batched=True,
-        )
+        data = data.map(lambda samples: self.tokenizer(samples["text"], truncation=True,
+                                                       max_length=self.max_length,
+                                                       padding=False,
+                                                       return_tensors=None, ), batched=True)
+
         return data.shuffle()
+
+
