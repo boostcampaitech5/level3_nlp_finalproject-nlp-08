2facd3aba4586426f71b0a688274a905eb729e2b
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2023-07-31 07:10:46 +0900

    fix: fix bugs
    setting null handler
    delete delay
    fix button error
    
    #50

--- frontend/src/components/SimilarPrecedentComponents/PrecedentCard.js
+++ frontend/src/components/SimilarPrecedentComponents/PrecedentCard.js
@@ -6,7 +6,7 @@ function PrecedentCard({ precedent, number }) {
             <p className="font-small text-gray-700 dark:text-gray-400">사건 번호 : {precedent.case_number}</p>
             <p className="font-small text-gray-700 dark:text-gray-400">사건 분류 : {precedent.case_type}</p>
             <p className="font-small text-gray-700 dark:text-gray-400">관련 법 조항 :{precedent.ref_article}</p>
-            <a href={precedent.url} target="_blank" className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href={precedent.url} target="_blank" rel="noopener noreferer nofollow" className="mt-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Read more
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
